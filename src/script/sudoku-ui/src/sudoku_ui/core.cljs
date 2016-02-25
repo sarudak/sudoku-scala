3582a756cd7157c142912cd189d199c6efecf33f
Sarudak <sarudak@gmail.com>
2016-02-24 16:15:34 -0800

    Can now render a basic sudoku board

--- src/script/sudoku-ui/src/sudoku_ui/core.cljs
+++ src/script/sudoku-ui/src/sudoku_ui/core.cljs
@@ -7,10 +7,36 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:board 
+[[0,0,6,0,0,7,3,0,0],
+ [0,1,8,0,0,9,0,5,0],
+ [5,0,0,0,0,0,0,6,4],
+ [9,2,0,0,8,0,0,0,0],
+ [0,0,0,7,6,3,0,0,0],
+ [0,0,0,0,9,0,0,7,5],
+ [6,3,0,0,0,0,0,0,8],
+ [0,9,0,3,0,0,5,2,0],
+ [0,0,2,4,0,0,6,0,0]] }))
+
+(defn render-cell [row-num col-num val]
+  [:div {:style {:width 40 :height 40 :float "left" :font-size "20pt"}} (str val)])
+
+(defn render-row [row-num row]
+  (map (partial render-cell row-num 1)
+   row))
+
+(defn render-board [board]
+  (map-indexed render-row 
+               board))
 
 (defn hello-world []
-  [:h1 (:text @app-state)])
+  [:div 
+   [:h1 "Let's play sudoku!"]
+   [:div {:style {:width 360 :height 360}}
+    
+    (render-board (:board @app-state))
+    ]
+  ])
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
